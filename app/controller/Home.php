49aafacda3672dd7ab9951faf459df01bf3401b1
Tim Spaans <foxtrot661@gmail.com>
2015-03-05 11:57:25 +0100

    Omgezet naar MVC

---
+++ app/controller/Home.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SysAdmin
+ * Date: 3/2/2015
+ * Time: 8:00 PM
+ */
+
+class Home extends Shared
+{
+    public function index()
+    {
+        $this->header('Home');
+        $this->menu();
+
+        $this->view('home/index');
+
+        $this->footer();
+    }
+
+    public function indextest($name = '')
+    {
+        $this->header('indextest');
+        $this->menu();
+
+        $user = $this->model('UserTest');
+        $user->name = $name;
+
+        $this->view('home/indextest', ['name' => $user->name]);
+
+        $this->footer();
+    }
+
+}
--- pages/home.php
+++ pages/home.php
@@ -1,88 +1,93 @@
-<!-- Header Carousel -->
-<header id="carousel" class="carousel slide" data-ride="carousel">
-	<!-- Indicators -->
-	<ol class="carousel-indicators">
-		<li data-target="#carousel" data-slide-to="0" class="active"></li>
-		<li data-target="#carousel" data-slide-to="1"></li>
-		<li data-target="#carousel" data-slide-to="2"></li>
-	</ol>
-	
-	<!-- Wrapper for slides -->
-	<div class="carousel-inner">
-		<div class="item active">
-			<img src="recources/img/slide1.jpg" alt="...">
-			<div class="carousel-caption">
-				<h3>Actueel Item 1</h3>
-			</div>
-		</div>
-		<div class="item">
-			<img src="recources/img/slide2.jpg" alt="...">
-			<div class="carousel-caption">
-				<h3>Actueel Item 2</h3>
-			</div>
-		</div>
-		<div class="item">
-			<img src="recources/img/slide3.jpg" alt="...">
-			<div class="carousel-caption">
-				<h3>Actueel Item 3</h3>
-			</div>
-		</div>
-	</div>
-	
-	<!-- Controls -->
-	<a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-		<span class="glyphicon glyphicon-chevron-left"></span>
-	</a>
-	<a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-		<span class="glyphicon glyphicon-chevron-right"></span>
-	</a>
-</header>
-<!-- Page Content -->
-<div class="container">
-	<!-- Features Section -->
-	<div class="row">
-		<div class="col-lg-12">
-			<h2 class="page-header">Nieuws</h2>
-		</div>
-		<div class="col-md-4">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h4>Nieuws Item 1</h4>
-				</div>
-				<div class="panel-body">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-					<a href="#" class="btn btn-default">Lees meer</a>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-4">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h4>Nieuws Item 2</h4>
-				</div>
-				<div class="panel-body">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-					<a href="#" class="btn btn-default">Lees meer</a>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-4">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h4>Nieuws Item 3</h4>
-				</div>
-				<div class="panel-body">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-					<a href="#" class="btn btn-default">Lees meer</a>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- /.row -->
 
-	<!-- Script to Activate the Carousel -->
-	<script>
-	$('.carousel').carousel({
-		interval: 5000 //changes the speed
-	})
-	</script>
+    <!-- Header Carousel -->
+    <header id="carousel" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+		  <ol class="carousel-indicators">
+			<li data-target="#carousel" data-slide-to="0" class="active"></li>
+			<li data-target="#carousel" data-slide-to="1"></li>
+			<li data-target="#carousel" data-slide-to="2"></li>
+		  </ol>
+		 
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner">
+			<div class="item active">
+			  <img src="recources/img/slide1.jpg" alt="...">
+			  <div class="carousel-caption">
+				  <h3>Actueel Item 1</h3>
+			  </div>
+			</div>
+			<div class="item">
+			  <img src="recources/img/slide2.jpg" alt="...">
+			  <div class="carousel-caption">
+				  <h3>Actueel Item 2</h3>
+			  </div>
+			</div>
+			<div class="item">
+			  <img src="recources/img/slide3.jpg" alt="...">
+			  <div class="carousel-caption">
+				  <h3>Actueel Item 3</h3>
+			  </div>
+			</div>
+		  </div>
+		 
+		  <!-- Controls -->
+		  <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+		  </a>
+		  <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+		  </a>
+    </header>
+
+   <!-- Page Content -->
+    <div class="container">
+
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Nieuws</h2>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Nieuws Item 1</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="#" class="btn btn-default">Lees meer</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Nieuws Item 2</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="#" class="btn btn-default">Lees meer</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Nieuws Item 3</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                        <a href="#" class="btn btn-default">Lees meer</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+
+
+            <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
