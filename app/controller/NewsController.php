4575c65a187af837975d0f06624607b4a959577f
Mehmet <94m.yildiz@gmail.com>
2015-03-11 19:52:48 +0100

    nieuws aanmaken
    - nieuws aanmaken werkt weer alleen voegt lege path toe.

--- app/controller/NewsController.php
+++ app/controller/NewsController.php
@@ -45,16 +45,65 @@ class NewsController extends Shared
         $this->footer();
     }
 
-    public function createNewsSave($create)
+    public function createNewsSaveOld()
     {
+        require_once '../app/repository/newsRepository.php';
+        require_once '../app/repository/fileRepository.php';
         require_once '../app/model/News.php';
-
-        echo '<h1>' . $create . '</h1>';
 
         $title = filter_var($_POST['title'], FILTER_SANITIZE_STRING);
         $content = filter_var($_POST['content'], FILTER_SANITIZE_STRING);
         $hidden = $_POST['hidden'];
         $districtsectionId = filter_var($_POST['district'], FILTER_VALIDATE_INT);
+
+        session_start();
+        $target = '../public/uploads/';
+        $filepaths = array();
+        $count = 0;
+        foreach($_FILES['file']['name'] as $filename)
+        {
+            $tmp = $_FILES['file']['tmp_name'][$count];
+            $count=$count + 1;
+            $target = $target.basename($filename);
+            $filepaths[] = $filename;
+            move_uploaded_file($tmp,$target);
+        }
+
+        if($hidden === true)
+        {
+            $hidden = 1;
+        }
+        else
+        {
+            $hidden = 0;
+        }
+
+        $news = new News(null, $districtsectionId, 1, $title, $content, new DateTime(), $hidden);
+        $newsrepo = new NewsRepository();
+        $newsId = $newsrepo->add($news);
+
+        $filerepo = new FileRepository();
+
+        foreach($filepaths as $path)
+        {
+            $filerepo->add($path, $newsId);
+        }
+
+        $this->header('Home');
+        $this->menu();
+        $this->view('home/index');
+        $this->footer();
+    }
+
+    public function createNewsSave($create)
+    {
+        require_once '../app/model/News.php';
+
+        $title = filter_var($_POST['title'], FILTER_SANITIZE_STRING);
+        $content = filter_var($_POST['content'], FILTER_SANITIZE_STRING);
+        $hidden = $_POST['hidden'];
+        $districtsectionId = filter_var($_POST['district'], FILTER_VALIDATE_INT);
+        $create = filter_var($create, FILTER_VALIDATE_BOOLEAN);
 
         //files worden geupload
         session_start();
@@ -112,7 +161,10 @@ class NewsController extends Shared
         //files worden toegevoegd aan db en gekoppeld aan nieuws
         foreach($filepaths as $path)
         {
-            $filerepo->add($path, $newsId);
+            if($path !== null)
+            {
+                $filerepo->add($path, $newsId);
+            }
         }
 
         $this->show($newsId);
