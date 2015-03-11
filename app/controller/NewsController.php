8d5cee6d2459a103f69127d33b997408fccbaed0
Mehmet <94m.yildiz@gmail.com>
2015-03-11 12:13:34 +0100

    merged met develop
    - merged develop into branch
    - aanpassing icon deel Corina
    - Opslaan en linken van files werkt

--- app/controller/NewsController.php
+++ app/controller/NewsController.php
@@ -21,6 +21,67 @@ class NewsController extends Shared
         $this->footer();
     }
 
-}
+    public function createNews()
+    {
+        require_once '../app/repository/districtsectionRepository.php';
 
-?>
+        $districtdb = new DistrictSectionRepository();
+
+        $this->header('Nieuw artikel');
+        $this->menu();
+        $this->view('news/createNews', $districtdb->getAll());
+        $this->footer();
+    }
+
+    public function createNewsSave()
+    {
+        require_once '../app/repository/fileRepository.php';
+        require_once '../app/model/News.php';
+
+        $title = filter_var($_POST['title'], FILTER_SANITIZE_STRING);
+        $content = filter_var($_POST['content'], FILTER_SANITIZE_STRING);
+        $hidden = $_POST['hidden'];
+        $districtsectionId = filter_var($_POST['district'], FILTER_VALIDATE_INT);
+
+        session_start();
+        $target = '../public/uploads/';
+        $filepaths = array();
+        $count = 0;
+        foreach($_FILES['file']['name'] as $filename)
+        {
+            $tmp = $_FILES['file']['tmp_name'][$count];
+            $count=$count + 1;
+            $target = $target.basename($filename);
+            $filepaths[] = $filename;
+            move_uploaded_file($tmp,$target);
+        }
+
+        if($hidden === true)
+        {
+            $hidden = 1;
+        }
+        else
+        {
+            $hidden = 0;
+        }
+
+        $news = new News(null, $districtsectionId, 1, $title, $content, new DateTime(), $hidden);
+        $newsrepo = new NewsRepository();
+
+        //voegt het artikel toe en krijgt een nieuws id terug, om deze aan eventuele bestanden toe te voegen.
+        $newsId = $newsrepo->add($news);
+
+        $filerepo = new FileRepository();
+
+        foreach($filepaths as $path)
+        {
+            $filerepo->add($path, $newsId);
+        }
+
+        $this->header('Home');
+        $this->menu();
+        $this->view('home/index');
+        $this->footer();
+    }
+
+}
