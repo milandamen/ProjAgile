311febee0a0a422c31e80228de802f79004e425f
milandamen <mdamen3@student.avans.nl>
2015-03-11 13:29:09 +0100

    Loshalen carousel en resize (v3)

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -1,45 +1,9 @@
+<header>
 
-<!-- Header Carousel -->
-<header id="carousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#carousel" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel" data-slide-to="1"></li>
-        <li data-target="#carousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-        <div class="item active">
-            <img src="/ProjAgile/public/img/slide1.jpg" alt="...">
-            <div class="carousel-caption">
-                <h3>Actueel Item 1</h3>
-            </div>
-        </div>
-        <div class="item">
-            <img src="/ProjAgile/public/img/slide2.jpg" alt="...">
-            <div class="carousel-caption">
-                <h3>Actueel Item 2</h3>
-            </div>
-        </div>
-        <div class="item">
-            <img src="/ProjAgile/public/img/slide3.jpg" alt="...">
-            <div class="carousel-caption">
-                <h3>Actueel Item 3</h3>
-            </div>
-        </div>
-    </div>
-
-    <!-- Controls -->
-    <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-    </a>
-    <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-    </a>
 </header>
-
 <!-- Page Content -->
+<?php require_once 'carousel.php'; ?>
+
 <div class="container">
 
     <!-- Features Section -->
