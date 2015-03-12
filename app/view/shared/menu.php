8b4d0dbfaacaaae0626e685f6045db2ca588d39b
hulkhogan <ronvasseldonk@gmail.com>
2015-03-12 18:22:00 +0100

    menu t/m submenu af en gekoppeld aan database
    base path moet nog ingebouwd worden zodat er niet steeds een stuk aan de
    url geplakt word.

--- app/view/shared/menu.php
+++ app/view/shared/menu.php
@@ -1,13 +1,80 @@
-<?php
-var_dump($data['menuData']);
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/ProjAgile/public/">
+            </a>
+        </div>
 
-?>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <?php
+                    $menuData = $data['menuData'];
 
-   <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+                    foreach($menuData as $menuItem)
+                    {
+                        if($menuItem->getParentId() == null)
+                        {
+                            //TODO add base url to path if not done => wijkraad/wijkraad/wijkraad/deelwijk/a
+                            $hasChildren = false;
+                            $parentItemCreated = false;
+                            foreach($menuData as $subMenuItem)
+                            {
+                                if($subMenuItem->getParentId() == $menuItem->getMenuId())
+                                {
+                                    $hasChildren = true;
+
+                                    if($parentItemCreated)
+                                    {
+                                        echo('<li><a href="' . $menuItem->getRelativeUrL() . '/' . $subMenuItem->getRelativeUrl() . '">'. $subMenuItem->getName() . '</a></li>');
+                                    }
+                                    else
+                                    {
+                                        $parentItemCreated = true;
+                                        echo('<li>');
+                                        echo(' <a href="' . $menuItem->getRelativeUrl() . '" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">'. $menuItem->getName() . '<span class="caret"></span></a>');
+                                        echo('<ul class="dropdown-menu" role="menu">');
+                                        echo('<li><a href="' . $menuItem->getRelativeUrl() . '/' . $subMenuItem->getRelativeUrl() . '">'. $subMenuItem->getName() . '</a></li>');
+                                    }
+
+                                }
+                            }
+                            if($hasChildren)
+                            {
+                                echo('</ul></li>');
+                            }
+                            else
+                            {
+                                echo('<li>');
+                                echo('<a href="'. $menuItem->getRelativeUrl() . '">' . $menuItem->getName() . '</a>');
+                                echo('</li>');
+                            }
+                        }
+                    }
+
+
+                ?>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+
+<!-- Navigation -->
+    <!--<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
+            <!--<div class="navbar-header">
                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
@@ -17,9 +84,9 @@ var_dump($data['menuData']);
                 <a class="navbar-brand" href="/ProjAgile/public/">
 				</a>
             </div>
-			
+
             <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <!--<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li>
                         <a href="/ProjAgile/public/">Wijkraad</a>
@@ -68,9 +135,9 @@ var_dump($data['menuData']);
                         </ul>
                     </li>
 					-->
-                </ul>
+                <!--</ul>
             </div>
             <!-- /.navbar-collapse -->
-        </div>
+        <!--</div>
         <!-- /.container -->
-    </nav>
+    <!--</nav>
