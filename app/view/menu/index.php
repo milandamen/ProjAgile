8c64f5e12f625794e496cc2039f84c29f04e6a92
hulkhogan <ronvasseldonk@gmail.com>
2015-03-16 20:43:25 +0100

    Menu table created

--- app/view/menu/index.php
+++ app/view/menu/index.php
@@ -3,17 +3,64 @@
 
 <?php
 $allMenuData = $data['allMenuData'];
-?>
 
+?>
 <div class="table-responsive">
     <table class="table">
         <thead>
         <tr>
-            <th data-field="id">Item ID</th>
-            <th data-field="name">Item Name</th>
-            <th data-field="price">Item Price</th>
+            <th data-field="id">#</th>
+            <th data-field="name">Menu-item naam</th>
+            <th data-field="url">pad naam</th>
+            <th data-field="menuOrder">Menu level</th>
+            <th data-field="publish">Tonen op site</th>
         </tr>
         </thead>
+        <tbody>
+            <?php
+                echo("<Form>");
+                foreach($allMenuData as $menuItem)
+                {
+                    echo("
+                        <tr>
+                            <td>" . $menuItem->getMenuId(). "</td>
+                            <td><input type='text' value='" . $menuItem->getName(). "'></td>
+                            <td><input type='text' value='" . $menuItem->getRelativeUrl(). "'></td>
+                    ");
+
+                    if($menuItem->getParentId() == null)
+                    {
+                        echo("
+                            <td><input type='text' value='" . $menuItem->getMenuOrder() . "'></td>
+                        ");
+                    }
+                    else
+                    {
+                        echo("
+                            <td><input type='text' value='" . $menuItem->getParentId() . '.' . $menuItem->getMenuOrder() . "'></td>
+                        ");
+                    }
+                    if($menuItem->getPublish() == 1)
+                    {
+                        echo("
+                                <td><input type='checkbox' checked></td>
+                            </tr>
+                        ");
+                    }
+                    else
+                    {
+                        echo("
+                                <td><input type='checkbox'></td>
+                            </tr>
+                        ");
+                    }
+                }
+                echo("</Form>");
+            ?>
+        </tbody>
     </table>
+    <?php
+        echo('<button type="button" style="float:right" class="btn btn-default">Opslaan</button>');
+    ?>
 </div>
 </table>
