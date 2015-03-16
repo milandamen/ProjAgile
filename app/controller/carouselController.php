95111c65ae44e4428323ac21100be01ad6940021
Mehmet <94m.yildiz@gmail.com>
2015-03-16 22:22:17 +0100

    begonnen aan uploaden

--- app/controller/carouselController.php
+++ app/controller/carouselController.php
@@ -5,4 +5,17 @@ class CarouselController extends Shared
     public function __construct(){
         parent::__construct();
     }
+
+    public function DemoView()
+    {
+        $this->header('carousel upload demo');
+        $this->menu();
+        $this->view('home/carouselUploadDemo');
+        $this->footer();
+    }
+
+    public function add()
+    {
+
+    }
 }
