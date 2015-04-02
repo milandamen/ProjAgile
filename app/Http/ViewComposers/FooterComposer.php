df31f846ae38614f758c992b89cf2e38c13f9f2d
milandamen <mdamen3@student.avans.nl>
2015-04-02 15:32:28 +0200

    Added ViewComposers folder

---
+++ app/Http/ViewComposers/FooterComposer.php
@@ -0,0 +1,27 @@
+<?php namespace App\Http\ViewComposers;
+
+use Illuminate\Contracts\View\View;
+use App\Http\Controllers\Controller;
+use App\Repository\FooterRepository;
+
+class FooterComposer {
+
+    public function __construct(FooterRepository $footerrepo)
+    {	
+    	$this->footerrepo = $footerrepo;
+    }
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  View  $view
+     * @return void
+     */
+
+    public function compose(View $view)
+    {
+    	$footer = $this->footerrepo->getAll();
+		$view->with('footer', $footer); 
+    }
+
+}
