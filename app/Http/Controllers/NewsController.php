76542f552f3bc04ac33ed7bc059637f024397f6f
milandamen <mdamen3@student.avans.nl>
2015-04-06 19:04:05 +0200

    Fixed file links

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -20,9 +20,10 @@ class NewsController extends Controller
             foreach($news->files as $file)
             {
                 $withoutId = substr($file->path, stripos($file->path, 'd') + 1);
+                $fileLinks[] = '<a href="' . action('FileController@getDownload') . '/' . $file->path . '">'. $withoutId . '</a><br/>';
             }
         }
-
-        return view('news/detail', $data = array('news' => $news, 'fileLinks' => $fileLinks));
+		
+		return view('news/detail', $data = array('news' => $news, 'fileLinks' => $fileLinks));
     }
 }
