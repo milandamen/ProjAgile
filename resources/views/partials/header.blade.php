ba73dd4d94476848c41b172ab37a4ca531dd4266
Leon Vermeulen <vermeulen1992>
2015-04-09 16:25:29 +0200

    menu tot zover

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -15,28 +15,37 @@
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#"><i class="fa fa-info"></i> Over Ons</a></li>
                 <li><a href="#"><i class="fa fa-paper-plane"></i> Contact</a></li>
+
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-search"></i></a>
+                    <ul class="dropdown-menu" style="padding:12px;">
+                        {!! Form::model(null, ['class' => 'navbar-form', 'role' => 'search']) !!}
+                            <div class="input-group">
+                                <div class="col-md-12" style="padding:0">
+                                {!! Form::text('query', null, ['placeholder' => 'Zoeken...', 'class' => 'form-control','style' => 'cols="20"']) !!}
+                                </div>
+                                    <div class="input-group-btn">
+                                        {!! Form::button('<i class="glyphicon glyphicon-search"></i>', ['type' => 'submit', 'class' => 'btn btn-default']) !!}
+                                    </div>
+
+                            </div>
+                        {!! Form::close() !!}
+                    </ul>
+                </li>
             </ul>
+
             <div class="span10">
                 <div class="row"></div>
-            <div>
-
-            <ul class="nav navbar-nav">
-                @foreach($menu as $subMenu)
-
-                    @if(isset($subMenu['sub']))
-
-                        @include('partials.menu.subItem', ['items' => $subMenu['sub'],'main' =>$subMenu])
-
-                    @else
-
-                        <li><a href="{!! $subMenu['main']->relativeUrl !!}">{!! $subMenu['main']->name !!}</a></li>
-
-                    @endif
-
-                @endforeach
-
-            </ul>
-
+                    <ul class="nav navbar-nav">
+                        @foreach($menu as $subMenu)
+                            @if(isset($subMenu['sub']))
+                                @include('partials.menu.subItem', ['items' => $subMenu['sub'],'main' =>$subMenu])
+                            @else
+                                <li><a href="{!! $subMenu['main']->relativeUrl !!}">{!! $subMenu['main']->name !!}</a></li>
+                            @endif
+                        @endforeach
+                    </ul>
+            </div>
         </div>
     </div>
 </nav>
