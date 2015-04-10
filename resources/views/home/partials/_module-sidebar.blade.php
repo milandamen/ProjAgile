96b2c473459a66a3e7bfd10849c6b8cb6430dad9
milandamen <mdamen3@student.avans.nl>
2015-04-10 22:09:03 +0200

    module-news fix

--- resources/views/home/partials/_module-sidebar.blade.php
+++ resources/views/home/partials/_module-sidebar.blade.php
@@ -1,30 +1,28 @@
-@section('sidebar')
-    <div class="panel panel-default dragdiv">								<!-- The dragdiv class is used in /Home/editlayour -->
-        <input class="hiddenInput" type="text" name="module-sidebar" />		<!-- This input gets sent in /Home/editlayout -->
-        <div class="panel-heading sidebar">
-            <h4>
-                {!! $sidebar[0]->title !!}
-                <!-- @\if()  logged in.-->
-                     <a class="right" href="{{ route('sidebar.edit', [$sidebar[0]->pageNr]) }}"><i class="fa fa-pencil-square-o"></i></a>
-                <!-- @\endif -->
-            </h4>
-        </div>
-        <div class="panel-body">
-            <ul>
-                @foreach($sidebar as $sidebarItem)
-                    @if(!$sidebarItem->extern)
-                            <li class="sidebar">
-                                <a href="{!! url($sidebarItem->link) !!}" class="">
-                                &gt; {!! $sidebarItem->text !!}</a>
-                            </li>
-                        @else
-                            <li class="sidebar">
-                                <a href=" {!! url($sidebarItem->link) !!}" target="_blank" class="">&gt;
-                                {!! $sidebarItem->text!!} </a>
-                            </li>
-                        @endif
-                @endforeach
-            </ul>
-        </div>
-    </div>
-@stop
+<div class="panel panel-default dragdiv">								<!-- The dragdiv class is used in /Home/editlayour -->
+	<input class="hiddenInput" type="text" name="module-sidebar" />		<!-- This input gets sent in /Home/editlayout -->
+	<div class="panel-heading sidebar">
+		<h4>
+			{!! $sidebar[0]->title !!}
+			<!-- @\if()  logged in.-->
+				 <a class="right" href="{{ route('sidebar.edit', [$sidebar[0]->pageNr]) }}"><i class="fa fa-pencil-square-o"></i></a>
+			<!-- @\endif -->
+		</h4>
+	</div>
+	<div class="panel-body">
+		<ul>
+			@foreach($sidebar as $sidebarItem)
+				@if(!$sidebarItem->extern)
+						<li class="sidebar">
+							<a href="{!! url($sidebarItem->link) !!}" class="">
+							&gt; {!! $sidebarItem->text !!}</a>
+						</li>
+					@else
+						<li class="sidebar">
+							<a href=" {!! url($sidebarItem->link) !!}" target="_blank" class="">&gt;
+							{!! $sidebarItem->text!!} </a>
+						</li>
+					@endif
+			@endforeach
+		</ul>
+	</div>
+</div>
