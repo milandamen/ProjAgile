21a7d904a58d9174774adeb6fd096fd26683c45b
Mehmet <94m.yildiz@gmail.com>
2015-04-12 16:52:56 +0200

    Meer dan 3 kolommen is niet meer toegestaan

--- public/custom/js/footerUpdate.js
+++ public/custom/js/footerUpdate.js
@@ -9,22 +9,31 @@ function addColumn()
     //set attributes
     table.setAttribute('class', 'col-sm-4');
     table.setAttribute('name', numTables);
-    //create edit buttons
-    var row1 = table.insertRow(0);
-    var row2 = table.insertRow(1);
-    var cell1 = row1.insertCell(0);
-    var cell2 = row2.insertCell(0);
-    cell1.innerHTML = '<button type="button" onclick="addRow(this)" class="btn btn-primary btn-sm">Voeg link toe</button> <button type="button" onclick="removeColumn(this)" class="btn btn-primary btn-sm">Verwijder kolom</button>';
-    cell2.innerHTML = '&nbsp;';
-    //create first link
-    var row = table.insertRow(2);
-    var cell = row.insertCell(0);
-    cell.innerHTML = 'Titel: <input type="text" name="footer[' + numTables + '][text][]" id="footerText" required>'+
-    '<button type="button" onclick="removeRow(this)" class="btn btn-primary btn-xs">X</button>'+
-    '<br/> Link: <input type="text" name="footer[' + numTables + '][link][]" id="footerLink">';
 
-    //finally, append table to container
-    container.appendChild(table);
+    if(numTables < 3)
+    {
+        //create edit buttons
+        var row1 = table.insertRow(0);
+        var row2 = table.insertRow(1);
+        var cell1 = row1.insertCell(0);
+        var cell2 = row2.insertCell(0);
+        cell1.innerHTML = '<button type="button" onclick="addRow(this)" class="btn btn-primary btn-sm">Voeg link toe</button> <button type="button" onclick="removeColumn(this)" class="btn btn-primary btn-sm">Verwijder kolom</button>';
+        cell2.innerHTML = '&nbsp;';
+        //create first link
+        var row = table.insertRow(2);
+        var cell = row.insertCell(0);
+        cell.innerHTML = 'Titel: <input type="text" name="footer[' + numTables + '][text][]" id="footerText" required>'+
+        '<button type="button" onclick="removeRow(this)" class="btn btn-primary btn-xs">X</button>'+
+        '<br/> Link: <input type="text" name="footer[' + numTables + '][link][]" id="footerLink">';
+
+        //finally, append table to container
+        container.appendChild(table);
+    }
+    else
+    {
+        alert('U kunt geen kolom meer toevoegen, maximaal aantal bereikt.');
+    }
+
 }
 
 function removeColumn(button)
