fd254a25ca8111ff6e37973e10a4de0cace49811
CN91 <c.nibbering@student.avans.nl>
2015-04-14 21:09:19 +0200

    Content beheerder versus administrator

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,6 +13,7 @@
                         <p> Op deze pagina zijn alle elementen te vinden die men kan beheren.</p>
                         <p> U heeft toegang tot alle onderdelen die hieronder te vinden zijn. Mocht u een onderdeel missen meld dit dan bij de beheerder!</p>
 
+                        @if(Auth::check() && Auth::user()->usergroup->name === 'Administrator')
                     <div class="col-lg-8 col-md-offset-2">
                        <div class="col-md-4">
 	           			<h3>Layout modules</h3>
@@ -42,6 +43,22 @@
 	           			</div>
                     
                     </div>
+                    @elseif(Auth::check() && Auth::user()->usergroup->name === 'Content Beheerder')
+                    <div class="col-md-4">
+                            <h3>Layout modules</h3>
+                            <div class="btn-group-vertical">
+                                    <a class="btn btn-default" href="{{ route('carousel.edit') }}" role="button">Carrousel wijzigen</a>
+                                    <a class="btn btn-default" href="{{ route('sidebar.edit', 1) }}" role="button">Sidebar Home wijzigen</a>
+                            </div>
+           			    </div>
+                        <div class="col-md-4">
+                            <h3>Content wijzigen</h3>
+                            <div class="btn-group-vertical">
+                                <a class="btn btn-default" href="#" role="button">Nieuws toevoegen</a>
+                                <a class="btn btn-default" href="{{ route('home.editIntroduction') }}" role="button">Introductie wijzigen</a>
+                            </div>
+                        </div>
+                        @endif
                 </div>
             </div>
     </div>
