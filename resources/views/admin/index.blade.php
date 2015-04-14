6ae0094f0c75561962fcb895cf4594c553e92539
CN91 <c.nibbering@student.avans.nl>
2015-04-14 14:12:00 +0200

    beheerpaneel updated
    beheerpanel updated, breadcrumbs updated

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
     <div class="container">
+    	<div class="row">
+				{!! Breadcrumbs::render('manage') !!}
+			</div>
+
         <!-- Features Section -->
             <div class="row">
                 <div class="col-lg-12">
@@ -13,9 +17,9 @@
                        <div class="col-md-4">
 	           			<h3>Layout modules</h3>
 	           				<div class="btn-group-vertical">
-	           					<a class="btn btn-default" href="{{ route('home.index') }}" role="button">Homepage layout </a>
+	           					<a class="btn btn-default" href="{{ route('home.editLayout') }}" role="button">Homepage layout </a>
 								<a class="btn btn-default" href="{{ route('carousel.edit') }}" role="button">Carrousel wijzigen</a>
-								<a class="btn btn-default" href="{{ route('sidebar.edit') }}" role="button">Sidebar Home wijzigen</a>
+								<a class="btn btn-default" href="{{ route('sidebar.edit', 1) }}" role="button">Sidebar Home wijzigen</a>
 								<a class="btn btn-default" href="{{ route('menu.index') }}" role="button">Menu wijzigen</a>
 								<a class="btn btn-default" href="{{ route('footer.edit') }}" role="button">Footer wijzigen</a>
 	           				</div>
@@ -24,7 +28,7 @@
 	           				<h3>Content wijzigen</h3>
 	           				<div class="btn-group-vertical">
 	           					<a class="btn btn-default" href="#" role="button">Nieuws toevoegen</a>
-	           					<a class="btn btn-default" href="home.editIntroduction" role="button">Introductie wijzigen</a>
+	           					<a class="btn btn-default" href="{{ route('home.editIntroduction')}}" role="button">Introductie wijzigen</a>
 	           				</div>
            				</div>
 
