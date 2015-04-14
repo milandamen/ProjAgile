c84f47ed2f04c51945599b502e38b418325bdfb6
hulkhogan <ronvasseldonk@gmail.com>
2015-04-14 11:07:20 +0200

    files deleted

--- resources/views/partials/scripts.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- jQuery -->
-{!! HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js') !!}
-
-<!-- Bootstrap Core JavaScript -->
-{!! HTML::script('bootstrap/js/bootstrap.js') !!}
