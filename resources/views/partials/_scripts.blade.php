c3f677930cb7a9083e6d65483b4f8460b1cb7736
CN91 <c.nibbering@student.avans.nl>
2015-04-24 15:05:16 +0200

    werkende summernote
    werkende summernote

--- resources/views/partials/_scripts.blade.php
+++ resources/views/partials/_scripts.blade.php
@@ -2,16 +2,14 @@
 {!! HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js') !!}
 {!! HTML::script('https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js') !!}
 
-<!-- Bootstrap Core JavaScript -->
-{!! HTML::script('bootstrap/js/bootstrap.js') !!}
-
 <!-- Multi level menu -->
 {!! HTML::script('custom/js/multilevel_menu.js') !!}
 
-
 <!-- include libries(jQuery, bootstrap, fontawesome) -->
 {!! HTML::script('https://code.jquery.com/jquery-1.9.1.min.js') !!}
-{!! HTML::script('https://netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js') !!} 
+
+<!-- Bootstrap Core JavaScript   (NEEDS TO BE AFTER JQUERY )-->
+{!! HTML::script('bootstrap/js/bootstrap.js') !!}
 
 <!-- include summernote js-->
 {!! HTML::script('summernote/js/summernote.min.js') !!}
