ab742f74426c6edb471e48363efdaa8d75eb0ed2
CN91 <c.nibbering@student.avans.nl>
2015-05-04 16:55:27 +0200

    naamwijzigng
    naamwijziging

--- public/custom/js/page.js
+++ public/custom/js/page.js
@@ -1,19 +1,19 @@
 var panelIndex = document.getElementById("newPanels").getElementsByTagName("div").length;
 document.getElementById('panelIndex').value = panelIndex;
 
-var div = document.getElementById('newPanels');
+var pagePanelsDiv = document.getElementById('newPanels');
 
 function newPanel(size){
 
-	var newDiv = document.createElement("div");
+	var newPanelDiv = document.createElement("div");
 	var label= '<h4 >Nieuw vak met grootte ' + size + '<a onclick="removePanel(this)" class="btn btn-danger btn-xs white"> Verwijder paneel</a></h4>';
 
 	var inputtitle = '<input type="text" class="form-control" placeholder="Titel" name="panel['+panelIndex+'][title]"/><br/>';
 	var inputcontent = '<textarea class="summernote" name="panel['+panelIndex+'][content]" placeholder="Inhoud"> </textarea>';
 	var hiddenfield = '<input type="number" name="panel['+panelIndex+'][size]"  value="'+size+'" hidden/>';
 
-	newDiv.innerHTML = label + inputtitle + inputcontent + hiddenfield;
-	div.appendChild(newDiv);
+	newPanelDiv.innerHTML = label + inputtitle + inputcontent + hiddenfield;
+	pagePanelsDiv.appendChild(newPanelDiv);
 
 	summer();
 	panelIndex++;
@@ -21,8 +21,8 @@ function newPanel(size){
 
 function removePanel(link){
 	
-	newDiv = link.parentNode.parentNode;
-	div.removeChild(newDiv);
+	oldPanelDiv = link.parentNode.parentNode;
+	pagePanelsDiv.removeChild(oldPanelDiv);
 }
 
 function validate(){
