cd823f6809c9a1b516dde00add48612e0ea9e58e
Mehmet <94m.yildiz@gmail.com>
2015-05-08 23:24:52 +0200

    probleem deels gefixed

--- public/custom/js/filterUserTables.js
+++ public/custom/js/filterUserTables.js
@@ -11,6 +11,8 @@ $('#search').keyup(function() {
     }).hide();
 });
 
+//CHANGE HREF AND ICON CLASS
+
 $('.activate').click(function (event){
 
     var clickedElement = $(this);
@@ -20,8 +22,32 @@ $('.activate').click(function (event){
     $.ajax({
         url: $(this).attr('href')
         ,success: function(response) {
+            console.log('succ 1');
             clickedElement.removeClass(activateClass);
+            clickedElement.removeClass('activate');
             clickedElement.addClass(deactivateClass);
+            clickedElement.addClass('deactivate');
+
+            $('.deactivate').click(function (event){
+
+                var clickedElement = $(this);
+
+                event.preventDefault();
+
+                $.ajax({
+                    url: $(this).attr('href')
+                    ,success: function(response) {
+                        console.log('succ 2');
+                        clickedElement.removeClass(deactivateClass);
+                        clickedElement.removeClass('deactivate');
+                        clickedElement.addClass(activateClass);
+                        clickedElement.addClass('activate');
+
+
+                    }
+                })
+                return false; //for good measure
+            });
         }
     })
 
@@ -37,9 +63,33 @@ $('.deactivate').click(function (event){
     $.ajax({
         url: $(this).attr('href')
         ,success: function(response) {
+            console.log('succ 2');
             clickedElement.removeClass(deactivateClass);
+            clickedElement.removeClass('deactivate');
             clickedElement.addClass(activateClass);
+            clickedElement.addClass('activate');
+
+            $('.activate').click(function (event){
+
+                var clickedElement = $(this);
+
+                event.preventDefault();
+
+                $.ajax({
+                    url: $(this).attr('href')
+                    ,success: function(response) {
+                        console.log('succ 1');
+                        clickedElement.removeClass(activateClass);
+                        clickedElement.removeClass('activate');
+                        clickedElement.addClass(deactivateClass);
+                        clickedElement.addClass('deactivate');
+                    }
+                })
+
+                return false; //for good measure
+            });
         }
     })
     return false; //for good measure
 });
+
