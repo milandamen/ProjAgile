319865626aa334e3b8405d88a60fd0bbb7a28f44
Ben van Doormalen <Bmdoorma@student.avans.nl>
2015-05-17 20:07:31 +0200

    Update _manageUsers.blade.php
    Inconsistency fix

--- resources/views/management/partials/_manageUsers.blade.php
+++ resources/views/management/partials/_manageUsers.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-3">
 	<h3>Gebruikers</h3>
 	<div class="btn-group-vertical">
-		{!! link_to_route('user.index', 'Gebruikers Overzicht', [], ['class' => 'btn btn-default']) !!}
+		{!! link_to_route('user.index', 'Gebruikers Beheer', [], ['class' => 'btn btn-default']) !!}
 		{!! link_to_route('user.create', 'Gebruiker Toevoegen', [], ['class' => 'btn btn-default']) !!}
 	</div>
 </div>
