0d5f897e72dea1555f53946f4264529234dd8aae
RobinVanBallegooij <rga.vanballegooij@student.avans.nl>
2015-05-18 16:47:08 +0200

    commit

--- resources/views/home/editlayout.blade.php
+++ resources/views/home/editlayout.blade.php
@@ -1,23 +1,33 @@
 @extends('app')
 
+@section('title')
+	De Bunders - Home Layout Wijzigen
+@stop
+
+@section('description')
+	Dit is de beveiligde home layout wijzig pagina van De Bunders.
+@stop
+
 @section('content')
-    <div class="container">
-    	    	<div class="row">
-				{!! Breadcrumbs::render('editlayout') !!}
-			</div>
+	<div class="container">
 		<div class="row">
-            <div class="col-md-12">
-                <h1>Wijzig home layout</h1>
-            </div>
-        </div>
+			{!! Breadcrumbs::render('home.editLayout') !!}
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Home Layouyt Wijzigen</h1>
+			</div>
+		</div>
+		<div class="row">
+			@include('flash::message')
+		</div>
 		<form id="dataform" method="post">
 			<input type="hidden" name="_token" value="{{ csrf_token() }}">
-			
 			<div id="draggabledivs" class="row">
 				{{-- Script for laying out modules on correct spots --}}
-				
 				<div class="col-md-8">
-					@for ($i = 0; $i < count($layoutModules) - 2; $i++)					{{-- Loop all modules, except the last one --}}
+					@for ($i = 0; $i < count($layoutModules) - 2; $i++)					
+						{{-- Loop all modules, except the last one --}}
 						@include('home.partials._' . $layoutModules[$i]->moduleName)
 					@endfor
 				</div>
@@ -25,20 +35,18 @@
 					@for ($i = count($layoutModules) - 2; $i < count($layoutModules); $i++)	
 						@include('home.partials._' . $layoutModules[$i]->moduleName)
 					@endfor
-            	</div>
-				
+				</div>
 				{{-- End layout script --}}
 			</div>
 		</form>
-		
 		<div>
+			<a class="btn btn-danger" onclick="location.href='{{ route('management.index') }}'">Annuleren</a>
 			<a class="btn btn-success" onclick="submitLayoutForm()">Opslaan</a>
-			<a class="btn btn-danger" onclick="location.href='{{route('admin.index')}}'">Annuleer</a>
 		</div>
 	</div>
-@endsection
+@stop
 
 @section('additional_scripts')
-	<!-- JavaScript that enables dragging and dropping and sends form -->
 	{!! HTML::script('custom/js/editlayout.js') !!}
-@endsection
+	{!! HTML::script('custom/js/flash_message.js') !!}
+@stop
