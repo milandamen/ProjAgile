f51d23dbfcc4789c3d6075194fb26d23cf992181
Mehmet <94m.yildiz@gmail.com>
2015-05-19 11:05:18 +0200

    Nog mee tabjes

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,22 +1,22 @@
 @if(count($footer) > 3)
-    <div class="container">
-        <!-- Footer -->
-        <hr>
-        <div style="background-color: {{$footer[3]->text}}" class="col-md-12 footer panel panel-default">
-            <!---1, because id 4 is for the color-->
-            @for($c = 0; $c < count($footer) - 1; $c++)
-                <div class="footerlist-container">
-                    <ul class="col-sm-4">
-                        {!!$footer[$c]->text!!}
-                    </ul>
-                </div>
-            @endfor
-            <div class="footer-edit-link">
-                @if(Auth::check() && Auth::user()->usergroup->name === 'Administrator')
-                        <a href="{{route('footer.edit')}}"><i class="fa fa-pencil-square-o"></i></a>
-                @endif
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<!-- Footer -->
+		<hr>
+		<div style="background-color: {{$footer[3]->text}}" class="col-md-12 footer panel panel-default">
+			<!---1, because id 4 is for the color-->
+			@for($c = 0; $c < count($footer) - 1; $c++)
+				<div class="footerlist-container">
+					<ul class="col-sm-4">
+						{!!$footer[$c]->text!!}
+					</ul>
+				</div>
+			@endfor
+			<div class="footer-edit-link">
+				@if(Auth::check() && Auth::user()->usergroup->name === 'Administrator')
+						<a href="{{route('footer.edit')}}"><i class="fa fa-pencil-square-o"></i></a>
+				@endif
+			</div>
+		</div>
+	</div>
 @endif
 
