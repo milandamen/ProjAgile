eac4e9e29c17c006cfa20508337dbfb57b7a4a2b
RobinVanBallegooij <rga.vanballegooij@student.avans.nl>
2015-05-20 13:46:19 +0200

    CreateUserRequest merge

--- app/Http/Requests/User/CreateUserRequest.php
+++ app/Http/Requests/User/CreateUserRequest.php
@@ -1,30 +1,30 @@
-<?php 
-	namespace App\Http\Requests\User;
+<?php
+namespace App\Http\Requests\User;
 
-	use App\Http\Controllers\UserController;
-	use App\Http\Requests\Request;
-	use Auth;
+use App\Http\Controllers\UserController;
+use App\Http\Requests\Request;
+use Auth;
 
-	class CreateUserRequest extends Request 
+class CreateUserRequest extends Request
+{
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
 	{
-		/**
-		 * Determine if the user is authorized to make this request.
-		 *
-		 * @return bool
-		 */
-		public function authorize()
-		{
-			return Auth::user()->usergroup->name === 'Administrator';
-		}
+		return Auth::user()->usergroup->name === 'Administrator';
+	}
 
-		/**
-		 * Get the validation rules that apply to the request.
-		 *
-		 * @return array
-		 */
-		public function rules()
-		{
-			$rules =
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		$rules =
 			[
 				'username' => 'required|max:30|unique:user,username',
 				'password' => 'required|confirmed|min:8',
@@ -34,70 +34,64 @@
 				'email' => 'required|max:60|email|unique:user,email',
 			];
 
-			//return rules based on userGroup.
-			//postal and houseNumber attributes are required only for residents.
-			if ((int)$this->get('userGroupId') === UserController::RESIDENT_GROUP_ID)
-			{
-				$rules +=
+		//return rules based on userGroup.
+		//postal and houseNumber attributes are required only for residents.
+		if ((int)$this->get('userGroupId') === UserController::RESIDENT_GROUP_ID) {
+			$rules +=
 				[
 					'houseNumber' => 'required|integer|digits_between:1,8',
 					'postal' => 'required|min:6|max:7|exists:postal,code',
 				];
-			}
-			else
-			{
-				$rules +=
+		} else {
+			$rules +=
 				[
 					'houseNumber' => 'integer|digits_between:1,8',
 					'postal' => 'min:6|max:7|exists:postal,code',
 				];
-			}
-
-			return $rules;
 		}
 
-		/**
-		 * Sanitizes the provided input that will be used by the validator and controller.
-		 *
-		 * @return array
-		 */
-		public function sanitize()
-		{
-			$input = $this->all();
-
-			$input['username'] = filter_var($input['username'], FILTER_SANITIZE_STRING);
-			$input['password'] = filter_var($input['password'], FILTER_SANITIZE_STRING);
-			$input['password_confirmation'] = filter_var($input['password_confirmation'], FILTER_SANITIZE_STRING);
-			$input['firstName'] = filter_var($input['firstName'], FILTER_SANITIZE_STRING);
-			$input['surname'] = filter_var($input['surname'], FILTER_SANITIZE_STRING);
-			$input['houseNumber'] = filter_var($input['houseNumber'], FILTER_SANITIZE_STRING);
-			$input['postal'] = filter_var($input['postal'], FILTER_SANITIZE_STRING);
-			$input['email'] = filter_var($input['email'], FILTER_SANITIZE_EMAIL);
-
-			$input['postal'] = $this->fixPostal($input['postal']);
-
-			$this->replace($input);
-
-			return $input;
-		}
-
-		/**
-		 * Fixes the postal input before it is used by the validator.
-		 *
-		 * @return string
-		 */
-		private function fixPostal($postal)
-		{
-			if (strlen($postal) > 7)
-			{
-				$postal = preg_replace('/\s+/', '', $postal);
-			}
-
-			if (strlen($postal) == 6)
-			{
-				$postal = substr_replace($postal, ' ', 4, 0);
-			}
-
-			return $postal;
-		}
+		return $rules;
 	}
+
+	/**
+	 * Sanitizes the provided input that will be used by the validator and controller.
+	 *
+	 * @return array
+	 */
+	public function sanitize()
+	{
+		$input = $this->all();
+
+		$input['username'] = filter_var($input['username'], FILTER_SANITIZE_STRING);
+		$input['password'] = filter_var($input['password'], FILTER_SANITIZE_STRING);
+		$input['password_confirmation'] = filter_var($input['password_confirmation'], FILTER_SANITIZE_STRING);
+		$input['firstName'] = filter_var($input['firstName'], FILTER_SANITIZE_STRING);
+		$input['surname'] = filter_var($input['surname'], FILTER_SANITIZE_STRING);
+		$input['houseNumber'] = filter_var($input['houseNumber'], FILTER_SANITIZE_STRING);
+		$input['postal'] = filter_var($input['postal'], FILTER_SANITIZE_STRING);
+		$input['email'] = filter_var($input['email'], FILTER_SANITIZE_EMAIL);
+
+		$input['postal'] = $this->fixPostal($input['postal']);
+
+		$this->replace($input);
+
+		return $input;
+	}
+
+	/**
+	 * Fixes the postal input before it is used by the validator.
+	 *
+	 * @return string
+	 */
+	private function fixPostal($postal)
+	{
+		if (strlen($postal) > 7) {
+			$postal = preg_replace('/\s+/', '', $postal);
+		}
+
+		if (strlen($postal) == 6) {
+			$postal = substr_replace($postal, ' ', 4, 0);
+		}
+		return $postal;
+	}
+}
