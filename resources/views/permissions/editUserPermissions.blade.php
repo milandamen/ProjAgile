f6e07ff4112056fc2d14289a4d6d2ca6be07b02f
RobinVanBallegooij <rga.vanballegooij@student.avans.nl>
2015-05-28 10:33:03 +0200

    permissions change view

--- resources/views/permissions/editUserPermissions.blade.php
+++ resources/views/permissions/editUserPermissions.blade.php
@@ -31,7 +31,7 @@
 		<div class="row">
 			<div class="col-xs-5">
 				<h3 class="text-center">Pagina's</h3>
-				<div class="well" style="max-height: 300px;overflow: auto;">
+				<div class="well" style="max-height: 600px;overflow: auto;">
 					<ul id="check-list-box-page" class="list-group checked-list-box">
 						@foreach($pages as $page)
 							<li class="list-group-item" id={{$page->pageId}}> {!! $page->pageId !!} - {!! $page->introduction->title !!}</li>
@@ -44,18 +44,35 @@
 			</div>
 
 			<div class="col-xs-5 col-xs-offset-2">
-				<h3 class="text-center">Nieuws per deelwijk</h3>
-				<div class="well" style="max-height: 300px;overflow: auto;">
-					<ul id="check-list-box-districtSection" class="list-group checked-list-box">
-						@foreach($districtSections as $districtSection)
-							<li class="list-group-item" id={{$districtSection->districtSectionId}}> {!! $districtSection->districtSectionId !!} - {!! $districtSection->name !!}</li>
-						@endforeach
-					</ul>
-					<br />
-					<button class="btn btn-primary col-xs-12" id="get-checked-data">Get Checked Data</button>
+				<div class="row">
+					<h3 class="text-center">Onderdelen</h3>
+					<div class="well" style="max-height: 300px;overflow: auto;">
+						<ul id="check-list-box-other" class="list-group checked-list-box">
+							<li class="list-group-item"> Homepage</li>
+							<li class="list-group-item"> Menu</li>
+							<li class="list-group-item"> Carousel</li>
+							<li class="list-group-item"> Footer</li>
+						</ul>
+					</div>
+					<pre id="display-json-other"></pre>
+				</div>
+
+				<div class="row">
+					<h3 class="text-center">Nieuws per deelwijk</h3>
+					<div class="well" style="max-height: 310px;overflow: auto;">
+						<ul id="check-list-box-districtSection" class="list-group checked-list-box">
+							@foreach($districtSections as $districtSection)
+								<li class="list-group-item" id={{$districtSection->districtSectionId}}> {!! $districtSection->districtSectionId !!} - {!! $districtSection->name !!}</li>
+							@endforeach
+						</ul>
+					</div>
+					<pre id="display-json-districtSection"></pre>
 				</div>
 			</div>
+
 		</div>
+
+
 
 		{!! link_to_route('user.index', 'Terug naar Gebruiker Beheer', [], ['class' => 'btn btn-danger']) !!}
 		{!! Form::submit('Opslaan', ['class' => 'btn btn-success']) !!}
