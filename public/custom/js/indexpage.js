868e0540aac5c794e0a350744ff4ab772ddeafe0
CN91 <c.nibbering@student.avans.nl>
2015-05-29 22:38:40 +0200

    sdd

--- public/custom/js/indexpage.js
+++ public/custom/js/indexpage.js
@@ -5,9 +5,7 @@ function controlPublic(item){
 	var id = $(item).find("i").attr("name");
 	url = url.replace("/0/", "/"+id+"/");
 	var icon =  $(item).find("i");
-		
-	console.log(icon);
-
+	
 	$.get(url).done(function() {
 		if ($(icon).hasClass('fa fa-eye')){
 			$(icon).removeClass('fa fa-eye').addClass('fa fa-eye-slash')
