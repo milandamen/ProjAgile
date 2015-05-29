7ccc49b66d02b1c26d9cd93218c5d93a7684a6b0
Milan Damen Linux <github-linux@darvit.nl>
2015-05-29 11:23:43 +0200

    Production optimization v2

---
+++ vendor/config.php
@@ -0,0 +1,370 @@
+<?php return array (
+  'app' => 
+  array (
+    'debug' => false,
+    'url' => 'http://localhost',
+    'timezone' => 'UTC',
+    'locale' => 'nl',
+    'fallback_locale' => 'en',
+    'key' => '0JXpSG6AN8zhmV7ByKaC5NBiyksRK70I',
+    'cipher' => 'rijndael-128',
+    'log' => 'daily',
+    'providers' => 
+    array (
+      0 => 'Illuminate\\Foundation\\Providers\\ArtisanServiceProvider',
+      1 => 'Illuminate\\Auth\\AuthServiceProvider',
+      2 => 'Illuminate\\Bus\\BusServiceProvider',
+      3 => 'Illuminate\\Cache\\CacheServiceProvider',
+      4 => 'Illuminate\\Foundation\\Providers\\ConsoleSupportServiceProvider',
+      5 => 'Illuminate\\Routing\\ControllerServiceProvider',
+      6 => 'Illuminate\\Cookie\\CookieServiceProvider',
+      7 => 'Illuminate\\Database\\DatabaseServiceProvider',
+      8 => 'Illuminate\\Encryption\\EncryptionServiceProvider',
+      9 => 'Illuminate\\Filesystem\\FilesystemServiceProvider',
+      10 => 'Illuminate\\Foundation\\Providers\\FoundationServiceProvider',
+      11 => 'Illuminate\\Hashing\\HashServiceProvider',
+      12 => 'Illuminate\\Mail\\MailServiceProvider',
+      13 => 'Illuminate\\Pagination\\PaginationServiceProvider',
+      14 => 'Illuminate\\Pipeline\\PipelineServiceProvider',
+      15 => 'Illuminate\\Queue\\QueueServiceProvider',
+      16 => 'Illuminate\\Redis\\RedisServiceProvider',
+      17 => 'Illuminate\\Auth\\Passwords\\PasswordResetServiceProvider',
+      18 => 'Illuminate\\Session\\SessionServiceProvider',
+      19 => 'Illuminate\\Translation\\TranslationServiceProvider',
+      20 => 'Illuminate\\Validation\\ValidationServiceProvider',
+      21 => 'Illuminate\\View\\ViewServiceProvider',
+      22 => 'App\\Providers\\AppServiceProvider',
+      23 => 'App\\Providers\\BusServiceProvider',
+      24 => 'App\\Providers\\ConfigServiceProvider',
+      25 => 'App\\Providers\\EventServiceProvider',
+      26 => 'App\\Providers\\RouteServiceProvider',
+      27 => 'App\\Providers\\ViewComposerServiceProvider',
+      28 => 'App\\Providers\\RepositoryServiceProvider',
+      29 => 'Illuminate\\Html\\HtmlServiceProvider',
+      30 => 'Laracasts\\Flash\\FlashServiceProvider',
+      31 => 'DaveJamesMiller\\Breadcrumbs\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'App' => 'Illuminate\\Support\\Facades\\App',
+      'Artisan' => 'Illuminate\\Support\\Facades\\Artisan',
+      'Auth' => 'Illuminate\\Support\\Facades\\Auth',
+      'Blade' => 'Illuminate\\Support\\Facades\\Blade',
+      'Bus' => 'Illuminate\\Support\\Facades\\Bus',
+      'Cache' => 'Illuminate\\Support\\Facades\\Cache',
+      'Config' => 'Illuminate\\Support\\Facades\\Config',
+      'Cookie' => 'Illuminate\\Support\\Facades\\Cookie',
+      'Crypt' => 'Illuminate\\Support\\Facades\\Crypt',
+      'DB' => 'Illuminate\\Support\\Facades\\DB',
+      'Eloquent' => 'Illuminate\\Database\\Eloquent\\Model',
+      'Event' => 'Illuminate\\Support\\Facades\\Event',
+      'File' => 'Illuminate\\Support\\Facades\\File',
+      'Hash' => 'Illuminate\\Support\\Facades\\Hash',
+      'Input' => 'Illuminate\\Support\\Facades\\Input',
+      'Inspiring' => 'Illuminate\\Foundation\\Inspiring',
+      'Lang' => 'Illuminate\\Support\\Facades\\Lang',
+      'Log' => 'Illuminate\\Support\\Facades\\Log',
+      'Mail' => 'Illuminate\\Support\\Facades\\Mail',
+      'Password' => 'Illuminate\\Support\\Facades\\Password',
+      'Queue' => 'Illuminate\\Support\\Facades\\Queue',
+      'Redirect' => 'Illuminate\\Support\\Facades\\Redirect',
+      'Redis' => 'Illuminate\\Support\\Facades\\Redis',
+      'Request' => 'Illuminate\\Support\\Facades\\Request',
+      'Response' => 'Illuminate\\Support\\Facades\\Response',
+      'Route' => 'Illuminate\\Support\\Facades\\Route',
+      'Schema' => 'Illuminate\\Support\\Facades\\Schema',
+      'Session' => 'Illuminate\\Support\\Facades\\Session',
+      'Storage' => 'Illuminate\\Support\\Facades\\Storage',
+      'URL' => 'Illuminate\\Support\\Facades\\URL',
+      'Validator' => 'Illuminate\\Support\\Facades\\Validator',
+      'View' => 'Illuminate\\Support\\Facades\\View',
+      'HTML' => 'Illuminate\\Html\\HtmlFacade',
+      'Form' => 'Illuminate\\Html\\FormFacade',
+      'Flash' => 'Laracasts\\Flash\\Flash',
+      'Breadcrumbs' => 'DaveJamesMiller\\Breadcrumbs\\Facade',
+    ),
+  ),
+  'auth' => 
+  array (
+    'driver' => 'eloquent',
+    'model' => 'App\\Models\\User',
+    'table' => 'user',
+    'password' => 
+    array (
+      'email' => 'emails.password',
+      'table' => 'password_resets',
+      'expire' => 60,
+    ),
+  ),
+  'cache' => 
+  array (
+    'default' => 'file',
+    'stores' => 
+    array (
+      'apc' => 
+      array (
+        'driver' => 'apc',
+      ),
+      'array' => 
+      array (
+        'driver' => 'array',
+      ),
+      'database' => 
+      array (
+        'driver' => 'database',
+        'table' => 'cache',
+        'connection' => NULL,
+      ),
+      'file' => 
+      array (
+        'driver' => 'file',
+        'path' => '/data/backupbunders/ProjAgile/storage/framework/cache',
+      ),
+      'memcached' => 
+      array (
+        'driver' => 'memcached',
+        'servers' => 
+        array (
+          0 => 
+          array (
+            'host' => '127.0.0.1',
+            'port' => 11211,
+            'weight' => 100,
+          ),
+        ),
+      ),
+      'redis' => 
+      array (
+        'driver' => 'redis',
+        'connection' => 'default',
+      ),
+    ),
+    'prefix' => 'laravel',
+  ),
+  'compile' => 
+  array (
+    'files' => 
+    array (
+      0 => '/data/backupbunders/ProjAgile/app/Providers/AppServiceProvider.php',
+      1 => '/data/backupbunders/ProjAgile/app/Providers/BusServiceProvider.php',
+      2 => '/data/backupbunders/ProjAgile/app/Providers/ConfigServiceProvider.php',
+      3 => '/data/backupbunders/ProjAgile/app/Providers/EventServiceProvider.php',
+      4 => '/data/backupbunders/ProjAgile/app/Providers/RouteServiceProvider.php',
+    ),
+    'providers' => 
+    array (
+    ),
+  ),
+  'database' => 
+  array (
+    'fetch' => 8,
+    'default' => 'mysql',
+    'connections' => 
+    array (
+      'sqlite' => 
+      array (
+        'driver' => 'sqlite',
+        'database' => '/data/backupbunders/ProjAgile/storage/database.sqlite',
+        'prefix' => '',
+      ),
+      'mysql' => 
+      array (
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'port' => '3306',
+        'database' => 'bunders',
+        'username' => 'bunders',
+        'password' => 'Koekje07',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
+        'strict' => false,
+      ),
+      'pgsql' => 
+      array (
+        'driver' => 'pgsql',
+        'host' => 'localhost',
+        'database' => 'bunders',
+        'username' => 'bunders',
+        'password' => 'Koekje07',
+        'charset' => 'utf8',
+        'prefix' => '',
+        'schema' => 'public',
+      ),
+      'sqlsrv' => 
+      array (
+        'driver' => 'sqlsrv',
+        'host' => 'localhost',
+        'database' => 'bunders',
+        'username' => 'bunders',
+        'password' => 'Koekje07',
+        'prefix' => '',
+      ),
+    ),
+    'migrations' => 'migrations',
+    'redis' => 
+    array (
+      'cluster' => false,
+      'default' => 
+      array (
+        'host' => '127.0.0.1',
+        'port' => 6379,
+        'database' => 0,
+      ),
+    ),
+  ),
+  'filesystems' => 
+  array (
+    'default' => 'local',
+    'cloud' => 's3',
+    'disks' => 
+    array (
+      'local' => 
+      array (
+        'driver' => 'local',
+        'root' => '/data/backupbunders/ProjAgile/storage/app',
+      ),
+      's3' => 
+      array (
+        'driver' => 's3',
+        'key' => 'your-key',
+        'secret' => 'your-secret',
+        'region' => 'your-region',
+        'bucket' => 'your-bucket',
+      ),
+      'rackspace' => 
+      array (
+        'driver' => 'rackspace',
+        'username' => 'your-username',
+        'key' => 'your-key',
+        'container' => 'your-container',
+        'endpoint' => 'https://identity.api.rackspacecloud.com/v2.0/',
+        'region' => 'IAD',
+        'url_type' => 'publicURL',
+      ),
+    ),
+  ),
+  'queue' => 
+  array (
+    'default' => 'sync',
+    'connections' => 
+    array (
+      'sync' => 
+      array (
+        'driver' => 'sync',
+      ),
+      'database' => 
+      array (
+        'driver' => 'database',
+        'table' => 'jobs',
+        'queue' => 'default',
+        'expire' => 60,
+      ),
+      'beanstalkd' => 
+      array (
+        'driver' => 'beanstalkd',
+        'host' => 'localhost',
+        'queue' => 'default',
+        'ttr' => 60,
+      ),
+      'sqs' => 
+      array (
+        'driver' => 'sqs',
+        'key' => 'your-public-key',
+        'secret' => 'your-secret-key',
+        'queue' => 'your-queue-url',
+        'region' => 'us-east-1',
+      ),
+      'iron' => 
+      array (
+        'driver' => 'iron',
+        'host' => 'mq-aws-us-east-1.iron.io',
+        'token' => 'your-token',
+        'project' => 'your-project-id',
+        'queue' => 'your-queue-name',
+        'encrypt' => true,
+      ),
+      'redis' => 
+      array (
+        'driver' => 'redis',
+        'queue' => 'default',
+        'expire' => 60,
+      ),
+    ),
+    'failed' => 
+    array (
+      'database' => 'mysql',
+      'table' => 'failed_jobs',
+    ),
+  ),
+  'session' => 
+  array (
+    'driver' => 'file',
+    'lifetime' => 120,
+    'expire_on_close' => false,
+    'encrypt' => false,
+    'files' => '/data/backupbunders/ProjAgile/storage/framework/sessions',
+    'connection' => NULL,
+    'table' => 'sessions',
+    'lottery' => 
+    array (
+      0 => 2,
+      1 => 100,
+    ),
+    'cookie' => 'laravel_session',
+    'path' => '/',
+    'domain' => NULL,
+    'secure' => false,
+  ),
+  'view' => 
+  array (
+    'paths' => 
+    array (
+      0 => '/data/backupbunders/ProjAgile/resources/views',
+    ),
+    'compiled' => '/data/backupbunders/ProjAgile/storage/framework/views',
+  ),
+  'mail' => 
+  array (
+    'driver' => 'smtp',
+    'host' => 'smtp.mijnhostingpartner.nl',
+    'port' => '25',
+    'from' => 
+    array (
+      'address' => 'noreply@debunders.nl',
+      'name' => 'De Bunders',
+    ),
+    'encryption' => '',
+    'username' => 'test@marinusit.com',
+    'password' => 'Koekje07!',
+    'sendmail' => '/usr/sbin/sendmail -bs',
+    'pretend' => false,
+  ),
+  'services' => 
+  array (
+    'mailgun' => 
+    array (
+      'domain' => '',
+      'secret' => '',
+    ),
+    'mandrill' => 
+    array (
+      'secret' => '',
+    ),
+    'ses' => 
+    array (
+      'key' => '',
+      'secret' => '',
+      'region' => 'us-east-1',
+    ),
+    'stripe' => 
+    array (
+      'model' => 'User',
+      'secret' => '',
+    ),
+  ),
+  'breadcrumbs' => 
+  array (
+    'view' => 'breadcrumbs::bootstrap3',
+  ),
+);
