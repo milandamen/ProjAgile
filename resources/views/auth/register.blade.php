59cd4f7eae0bf016fbb4ea20f5ea286d0d16df32
CN91 <c.nibbering@student.avans.nl>
2015-06-01 17:19:47 +0200

    registration view fix
    registration view fix en mail view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
 		{!! Breadcrumbs::render('auth.register') !!}
 		</div>
 		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
+			<div class="col-md-10 col-md-offset-1">
 				<h2 class="page-header">Registreren</h2>
 				<div class="row">
 					@include('flash::message')
@@ -23,81 +23,76 @@
 					<div class="panel-body">
 						@include('errors.partials._list')
 						{!! Form::open(['method' => 'POST','route' => 'auth.register']) !!}
-							<div class="form-group">
-								{!! Form::label('username', 'Gebruikersnaam:', ['class' => 'col-md-4 control-label']) !!}
-								<div class="col-md-6">
+							
+							<div class="col-md-12 no-padding addmargin">
+								<div class="col-md-12 addmargin">	
+									<div class="col-md-6">	
+									{!! Form::label('username', 'Gebruikersnaam:', ['class' => ' control-label']) !!}
 									{!! Form::text('username', old('username'), ['class' => 'form-control']) !!}
+									</div>
+								</div>
+								<div class="col-md-12 addmargin">
+									<div class="col-md-6">
+										{!! Form::label('password', 'Wachtwoord:', ['class' => 'control-label']) !!}
+										{!! Form::password('password', ['class' => 'form-control']) !!}
+									</div>
+									<div class="col-md-6">
+										{!! Form::label('password_confirmation', 'Herhaal Wachtwoord:', ['class' => 'control-label']) !!}
+										{!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+									</div>
+								</div>
+								<div class="col-md-12 addmargin">
+									<div class="col-md-6">
+										{!! Form::label('email', 'E-mailadres:', ['class' => 'control-label']) !!}
+										{!! Form::text('email', old('email'), ['class' => 'form-control']) !!}
+									</div>
+									<div class="col-md-6">
+										{!! Form::label('email_confirmation', 'Herhaal E-mailadres:', ['class' => 'control-label']) !!}
+										{!! Form::text('email_confirmation', old('email_confirmation'), ['class' => 'form-control']) !!}
+									</div>
+								</div>
+							</div>
+							<div class="col-md-12 form-group">
+								<div class="col-md-12 addmargin no-padding">
+									<div class="col-md-4">
+										{!! Form::label('firstName', 'Voornaam:', ['class' => 'control-label']) !!}
+										{!! Form::text('firstName', old('firstName'), ['class' => 'form-control']) !!}
+									</div>
+									<div class="col-md-3">
+										{!! Form::label('insertion', 'Tussenvoegsel:', ['class' => 'control-label']) !!}
+										{!! Form::text('insertion', old('insertion'), ['class' => 'form-control']) !!}
+									</div>
+									<div class="col-md-5">
+										{!! Form::label('surname', 'Achternaam:', ['class' => 'control-label']) !!}
+										{!! Form::text('surname', old('surname'), ['class' => 'form-control']) !!}
+									</div>
+								</div>
+
+								<div class="col-md-12 addmargin no-padding">
+									<div class="col-md-4">
+										{!! Form::label('houseNumber', 'Huisnummer', ['class' => 'control-label']) !!}
+										{!! Form::text('houseNumber', old('houseNumber'), ['class' => 'form-control']) !!}
+									</div>
+									<div class="col-md-3">
+										{!! Form::label('suffix', 'Toevoeging', ['class' => 'control-label']) !!}
+										{!! Form::text('suffix', old('suffix'), ['class' => 'form-control']) !!}
+									</div>
+									<div class="col-md-5">
+										{!! Form::label('postal', 'Postcode:', ['class' => 'ccontrol-label']) !!}
+										{!! Form::text('postal', old('postal'), ['class' => 'form-control']) !!}
+									</div>
 								</div>
 							</div>
 							<div class="form-group">
-								{!! Form::label('password', 'Wachtwoord:', ['class' => 'col-md-4 control-label']) !!}
-								<div class="col-md-6">
-									{!! Form::password('password', ['class' => 'form-control']) !!}
-								</div>
-							</div>
-							<div class="form-group">
-								{!! Form::label('password_confirmation', 'Herhaal Wachtwoord:', ['class' => 'col-md-4 control-label']) !!}
-								<div class="col-md-6">
-									{!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
-								</div>
-							</div>
-							<div class="form-group">
-								{!! Form::label('firstName', 'Voornaam:', ['class' => 'col-md-4 control-label']) !!}
-								<div class="col-md-6">
-									{!! Form::text('firstName', old('firstName'), ['class' => 'form-control']) !!}
-								</div>
-							</div>
-							<div class="form-group">
-								{!! Form::label('insertion', 'Tussenvoegsel:', ['class' => 'col-md-4 control-label']) !!}
-								<div class="col-md-6">
-									{!! Form::text('insertion', old('insertion'), ['class' => 'form-control']) !!}
-								</div>
-							</div>
-							<div class="form-group">
-								{!! Form::label('surname', 'Achternaam:', ['class' => 'col-md-4 control-label']) !!}
-								<div class="col-md-6">
-									{!! Form::text('surname', old('surname'), ['class' => 'form-control']) !!}
-								</div>
-							</div>
-							<div class="form-group">
-								{!! Form::label('houseNumber', 'Huisnummer', ['class' => 'col-md-4 control-label']) !!}
-								<div class="col-md-6">
-									{!! Form::text('houseNumber', old('houseNumber'), ['class' => 'form-control']) !!}
-								</div>
-							</div>
-							<div class="form-group">
-								{!! Form::label('suffix', 'Toevoeging', ['class' => 'col-md-4 control-label']) !!}
-								<div class="col-md-6">
-									{!! Form::text('suffix', old('suffix'), ['class' => 'form-control']) !!}
-								</div>
-							</div>
-							<div class="form-group">
-								{!! Form::label('postal', 'Postcode:', ['class' => 'col-md-4 control-label']) !!}
-								<div class="col-md-6">
-									{!! Form::text('postal', old('postal'), ['class' => 'form-control']) !!}
-								</div>
-							</div>
-							<div class="form-group">
-								{!! Form::label('email', 'E-mailadres:', ['class' => 'col-md-4 control-label']) !!}
-								<div class="col-md-6">
-									{!! Form::text('email', old('email'), ['class' => 'form-control']) !!}
-								</div>
-							</div>
-							<div class="form-group">
-								{!! Form::label('email_confirmation', 'Herhaal E-mailadres:', ['class' => 'col-md-4 control-label']) !!}
-								<div class="col-md-6">
-									{!! Form::text('email_confirmation', old('email_confirmation'), ['class' => 'form-control']) !!}
-								</div>
-							</div>
-							<div class="form-group">
-								<div class="col-md-4">
-								</div>
-								<div class="col-md-6">
+								<div class="col-md-12">
+									<div class="col-md-4 floatCenter addmargin">
 									{!! Recaptcha::render() !!}
 								</div>
+								</div>
+								
 							</div>
 							<div class="form-group">
-								<div class="col-md-6 col-md-offset-4">
+								<div class="floatRight">
 									{!! Form::submit('Registreren', ['class' => 'btn btn-primary']) !!}
 								</div>
 							</div>
