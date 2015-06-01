3720a7bf806aac006faee7239f6f6bd8cbd0cdb6
hulkhogan <ronvasseldonk@gmail.com>
2015-06-01 23:05:36 +0200

    View update

--- resources/views/postal/index.blade.php
+++ resources/views/postal/index.blade.php
@@ -13,10 +13,25 @@
         <div class="row">
             {{--{!! Breadcrumbs::render('news.edit', (object)['id' => $newsItem->newsId, 'title' => $newsItem->title]) !!}--}}
             {!! Breadcrumbs::render('postal.index') !!}
-            {!! Form::open(array('route' => 'postal.upload', 'files' => 'true')) !!}
 
-                {!! Form::file('Excel') !!}
-                {!! Form::submit('Bevestig') !!}
+            {!! Form::open(array('route' => 'postal.upload', 'files' => 'true')) !!}
+                <div class="panel panel-primary col-md-4 no-padding" style='margin-top:25px'>
+                    <div class="panel-heading">
+                        Upload Excel file
+                    </div>
+                    <div class="panel-body">
+                        <div class="form-group">
+                            <div>
+                                <div class="input-group">
+                                    {!! Form::file('Excel') !!}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group" style='margin-top:30px; float:right'>
+                            {!! Form::submit('Bevestig', ['class' => 'btn btn-success']) !!}
+                        </div>
+                    </div>
+                </div>
 
             {!! Form::close() !!}
 
