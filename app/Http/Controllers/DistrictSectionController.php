19b2e9bdff41751225bc2a3795a1a0645b9c891c
CN91 <c.nibbering@student.avans.nl>
2015-06-12 20:31:21 +0200

    commit
    commit

--- app/Http/Controllers/DistrictSectionController.php
+++ app/Http/Controllers/DistrictSectionController.php
@@ -3,6 +3,7 @@
 
 	use App\Repositories\RepositoryInterfaces\IDistrictSectionRepository;
 	use Auth;
+	use Illuminate\Support\Facades\Redirect;
 	use Input;
 	use App\Models\DistrictSection;
 	use App\Models\News;
@@ -32,4 +33,37 @@
 		public function index(){
 
 			$districts = $this->districtRepo->getAll();
+			return view('districtSection.index', compact('districts'));
+		}
 
+		public function show($name){
+			$district = $this->districtRepo->getByName($name);
+			
+			if(isset($district) && count($district)){
+					return view('districtSection.show', compact('district'));
+			}
+
+			return view('errors.404');
+		}
+
+		/**
+		 * Show the DistrictSection edit page.
+		 * 
+		 * @return Response
+		 */
+		public function edit($id)
+		{
+			
+		}
+
+		/**
+		 * Post the DistrictSection and handle the input.
+		 * 
+		 * @return Response
+		 */
+		public function update()
+		{
+
+		}
+		
+	}
