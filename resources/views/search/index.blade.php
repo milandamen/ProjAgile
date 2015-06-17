f96e6089a210dfd1b7b00668b6d9e5ac710ada0d
CN91 <c.nibbering@student.avans.nl>
2015-06-17 15:54:42 +0200

    Search view upgrade
    search view upgrade

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -16,66 +16,88 @@
 		<div class="row">
 			<h2 class="page-header">Zoekresultaten</h2>
 			@if(count($news))
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						Nieuws Resultaten
-					</div>
-					<div class="panel-body">
-						<table class="table">
-							@for($i = 0; $i < count($news); $i++)
-								<tr>
-									<a href="{{ route('news.show', [$news[$i]->newsId]) }}">{{ $news[$i]->title }}</a>
-									<br>
-									@if($i !== count($pages) -1)
-										<div class="addmargin">
-											{{ trunc(strip_tags($news[$i]->content), 50) }}
-										</div>
-									@else
-										{{ trunc(strip_tags($news[$i]->content), 50) }}
+				<div class="col-md-12">
+					<h4>Nieuws Resultaten</h4>
+					<p>Hieronder vindt u een overzicht van alle resultaten binnen de nieuws- afdeling van onze website. </p>
+					<table class="table">
+						<tr>
+							<th class="col-md-1"> Datum </th>
+							<th class="col-md-1"> Deelwijk </th>
+							<th class="col-md-2">Naam</th>
+							<th class="col-md-7">Inhoud</th>
+							<th class="col-md-1"><i class="fa fa-comment-o fa-lg floatRight"></i>  </th>
+						</tr>
+						@for($i = 0; $i < count($news); $i++)
+							<tr>
+								<td>{!!  $news[$i]->normalDate() !!} </td>
+								<td> @if(count($news[$i]->districtSections))
+									 {!! $news[$i]->districtSections[0]->name !!}
+									@if(count($news[$i]->districtSections) > 1)
+									,..
 									@endif
-								</tr>
-							@endfor
-						</table>
-					</div>
+								@else
+									Algemeen
+								@endif  
+									</td>
+								<td><a href="{{ route('news.show', [$news[$i]->newsId]) }}">{{ $news[$i]->title }}</a></td>
+								<td>
+									{{ trunc(strip_tags($news[$i]->content), 50) }}
+								</td>
+								<td>
+									<span class="floatRight">{!! count($news[$i]->newsComments) !!} </span>
+								</td>
+								<td><a href="{{ route('news.show', [$news[$i]->newsId]) }}"> 
+										<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>
+									</a> 
+								</td>
+							</tr>
+						@endfor
+					</table>
 				</div>
 			@endif
 			@if(count($pages))
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						Pagina Resultaten
-					</div>
-					<div class="panel-body">
-						<table class="table">
-							@for($i = 0; $i < count($pages); $i++)
-								<tr>
-									<a href="{{ route('page.show', [$pages[$i]->pageId]) }}">{{ $pages[$i]->introduction->title }}</a>
-									<br>
-									@if($i !== count($pages) -1)
-										<div class="addmargin">
-											{{ trunc(strip_tags($pages[$i]->introduction->text), 50) }}
-										</div>
+				<div class="col-md-12">
+					<h4>	Pagina Resultaten</h4>
+					<p>Hieronder vindt u een overzicht van alle resultaten binnen de pagina-afdeling van onze website. </p>
+						
+					<table class="table">
+						<tr>
+							<th class="col-md-1"> Deelwijk </th>
+							<th class="col-md-2">Naam</th>
+							<th class="col-md-7">Inhoud</th>
+						</tr>
+						@for($i = 0; $i < count($pages); $i++)
+							<tr>
+								<td>
+									 @if(count($pages[$i]->districtSections))
+									 {!! $pages[$i]->districtSections[0]->name !!}
+									@if(count($pages[$i]->districtSections) > 1)
+										,..
+										@endif
 									@else
-										{{ trunc(strip_tags($pages[$i]->introduction->text), 50) }}
-									@endif
-								</tr>
-							@endfor
-						</table>
-					</div>
+										Algemeen
+									@endif 
+								</td>
+								<td>
+									<a href="{{ route('page.show', [$pages[$i]->pageId]) }}">{{ $pages[$i]->introduction->title }}</a>
+								</td>
+								<td>
+									{{ trunc(strip_tags($pages[$i]->introduction->text), 50) }}
+								</td>
+								<td></td>
+							</tr>
+						@endfor
+					</table>
 				</div>
 			@endif
 			@if(!count($news) && !count($pages))
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						Geen Zoekresultaten
+				<div class="row">
+					<div class="col-md-12">
+						<h4> Geen Zoekresultaten </h4>
+						<p>Er zijn helaas geen resultaten gevonden op uw zoekopdracht.</p>
 					</div>
-					<div class="panel-body">
-						<div class="form-group">
-							<div class="col-md-12">
-								Er zijn helaas geen resultaten gevonden op uw zoekopdracht.
-							</div>
-						</div>
-					</div>
-				</div>	
+				</div>
+				
 			@endif
 		</div>
 	</div>
