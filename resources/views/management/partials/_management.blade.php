13d7ae8f7e3d1e59742813f2b5594deee8d5e754
milandamen <mdamen3@student.avans.nl>
2015-06-18 11:48:50 +0200

    Beheer panel geordend

--- resources/views/management/partials/_management.blade.php
+++ resources/views/management/partials/_management.blade.php
@@ -1,9 +1,9 @@
 <div class="col-md-3">
 	<h3>Beheer</h3>
 	<div class="btn-group-vertical">
-		{!! link_to_route('news.manage', 'Nieuws', [], ['class' => 'btn btn-default']) !!}
-		{!! link_to_route('postal.index', 'Postcodes ', [], ['class' => 'btn btn-default']) !!}
 		{!! link_to_route('district.manage', 'Deelwijken ', [], ['class' => 'btn btn-default']) !!}
+		{!! link_to_route('news.manage', 'Nieuws', [], ['class' => 'btn btn-default']) !!}
 		{!! link_to_route('page.index', 'Pagina\'s', [], ['class' => 'btn btn-default']) !!}
+		{!! link_to_route('postal.index', 'Postcodes ', [], ['class' => 'btn btn-default']) !!}
 	</div>
 </div>
