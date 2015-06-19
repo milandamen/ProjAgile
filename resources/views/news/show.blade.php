489c3b7cbda7d859b39a4522bcbf30f02cb9c3f5
Mehmet <94m.yildiz@gmail.com>
2015-06-19 13:38:55 +0200

    bug fix comments news

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -76,22 +76,7 @@
 			<div class="row">
 				<div class="col-lg-6">
 					@if(Auth::check())
-						@if($news->commentable || (Auth::user()->usergroup->name === 'Administrator' || Auth::user()->usergroup->name === 'Content Beheerder'))
-							@if(isset($news->districtSection))
-								@if(isset(Auth::user()->districtSection))
-									@if(Auth::user()->districtSection->name === $news->districtSection->name || 
-										(Auth::user()->usergroup->name === 'Administrator' || Auth::user()->usergroup->name === 'Content Beheerder'))
-										{!! Form::open(['route' => 'news.postComment', 'method' => 'POST']) !!}
-											<h3>Plaats een reactie</h3>
-											<div class="form-group">
-												<input type="hidden" name="newsId" value="{{$news->newsId}}">
-												<textarea name="comment" class="form-control"></textarea>
-											</div>
-											<button type="submit" class="btn btn-success" style="float: right;">Plaats reactie</button>
-										{!! Form::close() !!}
-									@endif
-								@endif
-							@else
+						@if($news->commentable)
 								{!! Form::open(['route' => 'news.postComment', 'method' => 'POST']) !!}
 									<h3>Plaats een reactie</h3>
 									<div class="form-group">
@@ -102,7 +87,6 @@
 								{!! Form::close() !!}
 							@endif
 						@endif
-					@endif
 				</div>
 			</div>
 		</div>
